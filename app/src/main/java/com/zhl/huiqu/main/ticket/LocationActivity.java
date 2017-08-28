419e9a1c5ede01c5584ccbb515b971fa2d080617
cd031116 <mm1314520>
2017-08-28 11:27:58 +0800

    1

--- app/src/main/java/com/zhl/huiqu/main/ticket/LocationActivity.java
+++ app/src/main/java/com/zhl/huiqu/main/ticket/LocationActivity.java
@@ -79,7 +79,7 @@ public class LocationActivity extends BaseActivity {
         LatLng point = new LatLng(Double.parseDouble(latitude),Double.parseDouble(longitude));
     //构建Marker图标
         BitmapDescriptor bitmap = BitmapDescriptorFactory
-                .fromResource(R.mipmap.ic_launcher);
+                .fromResource(R.mipmap.icon_openmap_mark);
         //构建MarkerOption，用于在地图上添加Marker
         OverlayOptions option = new MarkerOptions()
                 .position(point)
